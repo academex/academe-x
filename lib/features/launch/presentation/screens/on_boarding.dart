b215d08fb7e94c4e6b57c384e4bb815d144f8561
baraa <praa.m.2002@gmail.com>
2024-10-15 13:35:38 +0300

    created on boarding and privecy policy

---
+++ lib/features/launch/presentation/screens/on_boarding.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class OnBoarding extends StatelessWidget {
+  const OnBoarding({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
