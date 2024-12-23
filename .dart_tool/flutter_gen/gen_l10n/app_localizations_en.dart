0797c6cc5fbbe10fa4155702474649570d231e3f
Hussen <ghabayenhussej@gmail.com>
2024-12-23 12:34:18 +0200

    merge and update .ignor

---
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_en.dart
@@ -0,0 +1,206 @@
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get appTitle => 'AcademeX';
+
+  @override
+  String get loginTitle => 'Login';
+
+  @override
+  String get loginSubTitle => 'Please enter your information to log in to the world of knowledge and learning.';
+
+  @override
+  String get emailLabel => 'Email or username';
+
+  @override
+  String get emailHint => 'Enter email address';
+
+  @override
+  String get passwordLabel => 'Password';
+
+  @override
+  String get passwordHint => 'Enter password';
+
+  @override
+  String get forgotPassword => 'Forgot Password?';
+
+  @override
+  String get rememberMe => 'Remember me';
+
+  @override
+  String get loginButton => 'Login';
+
+  @override
+  String get orLoginWith => 'Or login using';
+
+  @override
+  String get googleAccount => 'Google Account';
+
+  @override
+  String get noAccount => 'Don’t have an account?';
+
+  @override
+  String get createAccount => 'Create Account';
+
+  @override
+  String get already_have_account => 'Already have an account?';
+
+  @override
+  String get forgotPasswordTitle => 'Forgot Password';
+
+  @override
+  String get forgotPasswordSubTitle => 'Don’t worry! that happens. Please specify your email or phone number so we can send you a code.';
+
+  @override
+  String get emailOption => 'Email';
+
+  @override
+  String get emailDescription => 'Email address: ';
+
+  @override
+  String get phoneOption => 'Phone Number';
+
+  @override
+  String get phoneDescription => 'Phone number: ';
+
+  @override
+  String get confirmationButton => 'Confirm';
+
+  @override
+  String get verificationTitle => 'Verification Code';
+
+  @override
+  String get verificationSubTitle => 'Please enter the verification code sent to your email:';
+
+  @override
+  String get resendCode => 'Resend code';
+
+  @override
+  String get newPasswordTitle => 'New Password';
+
+  @override
+  String get newPasswordSubTitle => 'The new password must be different from your previous password.';
+
+  @override
+  String get newPasswordLabel => 'New Password';
+
+  @override
+  String get confirmPasswordLabel => 'Confirm Password';
+
+  @override
+  String get passwordRequirement1 => 'Your password must be';
+
+  @override
+  String get passwordMinimumChars => 'at least 8 characters';
+
+  @override
+  String get passwordRequirement2 => 'containing some words and phrases to make it more secure.\"';
+
+  @override
+  String get robotIntroTitle => 'Hello, I\'m the Smart Robot';
+
+  @override
+  String get robotIntroSubTitle => 'I\'ll help you create accounts and prepare your data with great options and levels.';
+
+  @override
+  String get startButton => 'Start';
+
+  @override
+  String get createAccountTitle => 'Continue with Google Account';
+
+  @override
+  String get orCreateAccount => 'or Create my Account';
+
+  @override
+  String get accountPreparationMessage => 'Preparing your account will take a few seconds!';
+
+  @override
+  String get personalData => 'Personal Data';
+
+  @override
+  String get educationalLevel => 'Educational Level';
+
+  @override
+  String get nameLabel => 'Name';
+
+  @override
+  String get nameHint => 'Enter your name';
+
+  @override
+  String get collegeLabel => 'College';
+
+  @override
+  String get currentYearLabel => 'Current Study Year';
+
+  @override
+  String get semesterLabel => 'Study Semester';
+
+  @override
+  String get createAccountButton => 'Create Account';
+
+  @override
+  String get accountCreationLoadingTitle => 'Creating Your Account';
+
+  @override
+  String get accountCreationLoadingSubTitle => 'Your account and library are being prepared based on your level';
+
+  @override
+  String get accountCreationSuccessTitle => 'Account Created Successfully!';
+
+  @override
+  String get accountCreationSuccessSubTitle => 'Your account has been set up successfully.';
+
+  @override
+  String get redirectIn => 'Redirecting to the main page in ';
+
+  @override
+  String get seconds => 'seconds';
+
+  @override
+  String get goToMainPage => 'Go to Main Page';
+
+  @override
+  String get skip => 'Skip';
+
+  @override
+  String get onboarding_title_1 => 'Academy Chat: AI-powered assistant for students';
+
+  @override
+  String get onboarding_title_2 => 'A dedicated community for each college for questions and help';
+
+  @override
+  String get onboarding_title_3 => 'Comprehensive library of educational materials, summaries, and videos';
+
+  @override
+  String get onboarding_desc_1 => 'The Academy Chat answers all academic questions, summarizes files, and generates educational questions.';
+
+  @override
+  String get onboarding_desc_2 => 'A space for college students to discuss, ask questions, conduct polls, and share the latest university news.';
+
+  @override
+  String get onboarding_desc_3 => 'An electronic library containing study materials and summaries for all subjects.';
+
+  @override
+  String get previous => 'Previous';
+
+  @override
+  String get next => 'Next';
+
+  @override
+  String get start_now => 'Start Now';
+
+  @override
+  String get terms_and_conditions => 'Using the app implies agreement to our';
+
+  @override
+  String get privacy_policy => 'Terms of Service and Privacy Policy';
+
+  @override
+  String get privacy_policy_suffix => '.';
+}
